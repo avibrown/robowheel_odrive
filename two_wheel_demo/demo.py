d7841cbb719a3a32983ee4f7232bc64677f31411
avi <63222803+avielbr@users.noreply.github.com>
2022-10-07 15:00:56 +0300

    added demo

---
+++ two_wheel_demo/demo.py
@@ -0,0 +1,38 @@
+import math
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import Float32
+
+
+class ODriveDemoNode(Node):
+
+    def __init__(self):
+        super().__init__('odrive_demo_publisher')
+        self.axis0_publisher = self.create_publisher(Float32, 'axis0_vel_sub', 10)
+        self.axis1_publisher = self.create_publisher(Float32, 'axis1_vel_sub', 10)
+        
+        timer_period = 1 / 50
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = Float32()
+        msg.data = math.cos(self.i)
+        self.axis0_publisher.publish(msg)
+
+        msg.data = math.sin(self.i)
+        self.axis1_publisher.publish(msg)
+
+        self.i += (math.pi / 100)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    odrive_demo_publisher = ODriveDemoNode()
+
+    rclpy.spin(odrive_demo_publisher)
+    odrive_demo_publisher.destroy_node()
+    rclpy.shutdown()
+
